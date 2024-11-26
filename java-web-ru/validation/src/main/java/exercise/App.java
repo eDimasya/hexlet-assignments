8fffb1049fd091c24c7ade882d71407eb2241514
@hexlet/cli <support@hexlet.io>
2024-11-26 22:34:41 +0100

    submit java-web-ru/validation

--- java-web-ru/validation/src/main/java/exercise/App.java
+++ java-web-ru/validation/src/main/java/exercise/App.java
@@ -57,7 +57,8 @@ public final class App {
                 ctx.redirect("/articles");
             } catch (ValidationException e) {
                 BuildArticlePage page = new BuildArticlePage(e.getErrors());
-                ctx.render("articles/build.jte", model("page", page));
+                ctx.status(422)
+                        .render("articles/build.jte", model("page", page));
             }
         });
         // END
