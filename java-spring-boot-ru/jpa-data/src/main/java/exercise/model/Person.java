3bdb5cf256c615af8a8d40bd15dd38b17a52d242
@hexlet/cli <support@hexlet.io>
2025-01-09 17:15:27 +0100

    submit java-spring-boot-ru/jpa-data

---
+++ java-spring-boot-ru/jpa-data/src/main/java/exercise/model/Person.java
@@ -0,0 +1,29 @@
+package exercise.model;
+
+import jakarta.persistence.*;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+// BEGIN
+@Entity
+@Table(name = "persons")
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Person {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+}
+// END
