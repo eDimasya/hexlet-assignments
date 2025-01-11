fccc972af83509a197970489ab0b50d01c0689f4
@hexlet/cli <support@hexlet.io>
2025-01-12 00:44:10 +0100

    submit java-spring-boot-ru/jpa-repository

---
+++ java-spring-boot-ru/jpa-repository/src/main/java/exercise/controller/ProductsController.java
@@ -0,0 +1,52 @@
+package exercise.controller;
+
+import jakarta.websocket.server.PathParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+
+import exercise.model.Product;
+import exercise.repository.ProductRepository;
+import exercise.exception.ResourceNotFoundException;
+
+@RestController
+@RequestMapping("/products")
+public class ProductsController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    // BEGIN
+    @GetMapping
+    public List<Product> index(@PathParam(value = "") String min,
+                               @PathParam(value = "") String max) {
+        Integer minPrice = (min != null) ? Integer.parseInt(min) : null;
+        Integer maxPrice = (max != null) ? Integer.parseInt(max) : null;
+
+        if (minPrice != null && maxPrice != null) {
+            return productRepository.findByPriceBetweenOrderByPrice(minPrice, maxPrice);
+        } else if (minPrice != null) {
+            return productRepository.findByPriceGreaterThanOrderByPrice(minPrice);
+        } else if (maxPrice != null) {
+            return productRepository.findByPriceLessThanOrderByPrice(maxPrice);
+        } else {
+            return productRepository.findAll(Sort.by(Sort.Order.by("price")));
+        }
+    }
+    // END
+
+    @GetMapping(path = "/{id}")
+    public Product show(@PathVariable long id) {
+
+        var product =  productRepository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException("Product with id " + id + " not found"));
+
+        return product;
+    }
+}
---
+++ java-spring-boot-ru/jpa-repository/src/main/java/exercise/repository/ProductRepository.java
@@ -0,0 +1,17 @@
+package exercise.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import exercise.model.Product;
+
+import org.springframework.data.domain.Sort;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    // BEGIN
+    List<Product> findByPriceBetweenOrderByPrice(int min, int max);
+    List<Product> findByPriceLessThanOrderByPrice(int max);
+    List<Product> findByPriceGreaterThanOrderByPrice(int min);
+    // END
+}
