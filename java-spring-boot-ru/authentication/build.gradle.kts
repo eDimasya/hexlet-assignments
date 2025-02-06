b7beba21c138761bbef24e68293c6702350fec27
@hexlet/cli <support@hexlet.io>
2025-02-06 18:46:31 +0100

    submit java-spring-boot-ru/authentication

--- java-spring-boot-ru/authentication/build.gradle.kts
+++ java-spring-boot-ru/authentication/build.gradle.kts
@@ -36,6 +36,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
     testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("com.h2database:h2")
     // END
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
